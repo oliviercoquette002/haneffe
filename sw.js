b3a5dc88343373366ab7a2e28b652ca17543c334
oliviercoquette002 <olivier.coquette@gmail.com>
2026-06-10 13:42:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE = 'haneffe-v2';
+const CACHE = 'haneffe-v3';
 const ASSETS = ['/haneffe/', '/haneffe/index.html', '/haneffe/manifest.json', '/haneffe/icon.svg'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); });
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
+ 
