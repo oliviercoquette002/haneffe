46736e64c08886356b289b68188b3742986d671e
oliviercoquette002 <olivier.coquette@gmail.com>
2026-06-10 13:49:48 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE = 'haneffe-v3';
-const ASSETS = ['/haneffe/', '/haneffe/index.html', '/haneffe/manifest.json', '/haneffe/icon.svg'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); });
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
- 
